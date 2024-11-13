80fdc0a0e9e3886388a12d9d846734b52dc40a78
DeguzBelarus <deguz@mail.ru>
2024-11-13 17:35:26 +0300

    feat: install mogoose module; add db access string constant

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { HttpExceptionFilter } from './exception-filter/exception-filter';
 import { WebsocketsModule } from './websockets/websockets.module';
+import { MONGO_DB_ACCESS_STRING } from './constants';
 
 @Module({
-  imports: [ConfigModule.forRoot(), WebsocketsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    WebsocketsModule,
+    MongooseModule.forRoot(MONGO_DB_ACCESS_STRING),
+  ],
   controllers: [],
   providers: [
     {
--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,6 @@ config();
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 export const PORT = process.env.PORT || 5000;
 export const EMPTY_STRING = '';
+const DB_USER_NAME = process.env.db_user_name;
+const DB_USER_PASSWORD = process.env.db_user_password;
+export const MONGO_DB_ACCESS_STRING = `mongodb+srv://${DB_USER_NAME}:${DB_USER_PASSWORD}@paralect-db.liqam.mongodb.net/`;
